a668c16438a2b18e51f803c31afed896ada92542
rambabusure <rambabusure@gmail.com>
2018-01-18 12:57:11 -0500

    updated new methods for predict results

--- kfoldvalidation.py
+++ kfoldvalidation.py
@@ -6,6 +6,7 @@ Created on Mon Jan  8 13:09:01 2018
 """
 import numpy as np
 from sklearn.cross_validation import KFold
+from sklearn.externals import joblib
 
 
 
@@ -25,3 +26,22 @@ def run_cv(X,y,clf_class, **kwargs):
 
 def accuracy(y_true, y_pred):
     return np.mean(y_true == y_pred)
+
+# this method will run and presist the model based on the high accuracy model
+def generateandpersistmodel(X,y,clf_class):
+    kf = KFold(len(y),n_folds=3, shuffle=True)
+    for train_index,test_index in kf:
+        X_train = X[train_index]
+        y_train=  y[train_index]
+        clf = clf_class()
+        clf.fit(X_train,y_train)
+        joblib.dump(clf,'bankchurnmodel.pkl')   
+    return
+
+#  this method will load the persisted model and return the predicted results.
+def predictresults(inputdata):
+    clf = joblib.load('bankchurnmodel.pkl')
+    y_pred=  clf.predict(inputdata)
+    return y_pred
+    
+    
--- modelEvaulator.py
+++ modelEvaulator.py
@@ -17,20 +17,8 @@ from sklearn.ensemble import GradientBoostingClassifier as GBC
 
 
 
-def modelevaluator():
-    churn_df = pd.read_csv("churn.csv")
-    churn_result = churn_df['Churn?']
-    y= np.where(churn_result =='True.',1,0)
-    # We don't need these columns
-    to_drop = ['State','Area Code','Phone','Churn?']
-    churn_feat_space =  churn_df.drop(to_drop, axis=1)
-    # 'yes'/'no' has to be converted to boolean values
-    # NumPy converts these from boolean to 1. and 0. later
-    yes_no_cols = ["Int'l Plan","VMail Plan"]
-    
-    churn_feat_space[yes_no_cols] = churn_feat_space[yes_no_cols] == 'yes'
-    
-    X = churn_feat_space.as_matrix().astype(np.float)
+def modelevaluator(datafilename):
+    X,y =  formatdata(datafilename)
     scaler = StandardScaler()
     X = scaler.fit_transform(X)
     model_accuracy_values = {}
@@ -47,3 +35,38 @@ def modelevaluator():
     return max(model_accuracy_values, key=lambda k: model_accuracy_values[k]) 
 
 
+def formatdata(datafilename):
+    churn_df = pd.read_csv(datafilename)
+    churn_result = churn_df['Churn?']
+    y= np.where(churn_result =='True.',1,0)
+    # We don't need these columns
+    to_drop = ['State','Area Code','Phone','Churn?']
+    churn_feat_space =  churn_df.drop(to_drop, axis=1)
+    # 'yes'/'no' has to be converted to boolean values
+    # NumPy converts these from boolean to 1. and 0. later
+    yes_no_cols = ["Int'l Plan","VMail Plan"]
+    churn_feat_space[yes_no_cols] = churn_feat_space[yes_no_cols] == 'yes'
+    X = churn_feat_space.as_matrix().astype(np.float)
+    return (X,y)
+
+
+def formatinputdata(datafilename):
+    churn_df = pd.read_csv(datafilename)
+   
+    # We don't need these columns
+    to_drop = ['State','Area Code','Phone','Churn?']
+    churn_feat_space =  churn_df.drop(to_drop, axis=1)
+    # 'yes'/'no' has to be converted to boolean values
+    # NumPy converts these from boolean to 1. and 0. later
+    yes_no_cols = ["Int'l Plan","VMail Plan"]
+    churn_feat_space[yes_no_cols] = churn_feat_space[yes_no_cols] == 'yes'
+    X = churn_feat_space.as_matrix().astype(np.float)
+    return X
+
+accuratemodel = modelevaluator('churn.csv')
+print(accuratemodel)
+X,y = formatdata('churn.csv')
+kv.generateandpersistmodel(X,y,GBC)
+X_Input,y = formatinputdata('churn_predict.csv')
+y_pred= kv.predictresults(X_Input.reshape(1,-1))
+print(y_pred)
